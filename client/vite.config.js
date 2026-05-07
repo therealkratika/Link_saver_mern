3dc0d4aad928018dc86a1a07b748b7340e6b8d15
Kratika Gupta <therealkratikagupta@gmail.com>
2026-05-07 23:59:18 +0530

    deploymwnt setup

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
+import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    
-    react(),
-    babel({ presets: [reactCompilerPreset()] })
-  ],
+  plugins: [react()],
+
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    allowedHosts: true
+  }
 })
