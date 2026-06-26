47c6cbfc0dbf9383a175c1262248eac162a1157f
therealkratika <therealkratikagupta@gmail.com>
2026-06-26 13:47:55 +0530

    feat: app.jsx routing issue fixing.

--- server/utils/sendMail.js
+++ server/utils/sendMail.js
@@ -1,54 +1,28 @@
-const { Resend } = require("resend");
+const sendVerificationEmail = async (email, token) => {
+  const verificationLink =
+    `${process.env.BASE_URL}/api/auth/verify/${token}`;
 
-const resend = new Resend(
-  process.env.RESEND_API_KEY
-);
-
-const sendVerificationEmail =
-  async (email, token) => {
-
-    const verificationLink =
-      `${process.env.BASE_URL}/api/auth/verify/${token}`;
-
-    await resend.emails.send({
+  try {
+    const response = await resend.emails.send({
       from: "onboarding@resend.dev",
       to: email,
       subject: "Verify Your Email",
-
       html: `
         <h2>Email Verification</h2>
+
+        <p>Click the button below to verify your email:</p>
 
         <a href="${verificationLink}">
           Verify Email
         </a>
       `,
     });
-};
 
-const sendResetPasswordEmail =
-  async (email, token) => {
+    console.log("RESEND RESPONSE:", response);
 
-    const resetLink =
-      `https://link-saver-mern-914j.onrender.com/reset-password/${token}`;
-
-    await resend.emails.send({
-      from: "onboarding@resend.dev",
-      to: email,
-      subject: "Reset Your Password",
-
-      html: `
-        <h2>Password Reset</h2>
-
-        <p>Click below to reset password:</p>
-
-        <a href="${resetLink}">
-          Reset Password
-        </a>
-      `,
-    });
-};
-
-module.exports = {
-  sendVerificationEmail,
-  sendResetPasswordEmail,
+    return response;
+  } catch (error) {
+    console.error("RESEND ERROR:", error);
+    throw error;
+  }
 };
